b89763866c3900d1002143f3c36e8cdb1a4e9c33
5431 <29852058@qq.com>
2019-04-30 12:51:10 +0800

    Site updated: 2019-04-30 12:51:04

---
+++ dist/music.js
@@ -0,0 +1,39 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: false,
+    autoplay: true,
+    loop: all,
+    order: list,
+    audio: [
+      {
+        name: "美好事物",
+        artist: '房东的猫',
+        url: 'https://www.5431.name/music/1.mp3',
+        cover: 'https://www.5431.name/music/1.jpg',
+      },
+      {
+        name: '云烟成雨',
+        artist: '房东的猫',
+        url: 'https://www.5431.name/music/2.mp3',
+        cover: 'https://www.5431.name/music/1.jpg',
+      },
+      {
+        name: '秋酿',
+        artist: '房东的猫',
+        url: 'https://www.5431.name/music/3.mp3',
+        cover: 'https://www.5431.name/music/1.jpg',
+      },
+      {
+        name: '下一站茶山刘',
+        artist: '房东的猫',
+        url: 'https://www.5431.name/music/4.mp3',
+        cover: 'https://www.5431.name/music/1.jpg',
+      },
+      {
+        name: '春风十里',
+        artist: '房东的猫',
+        url: 'https://www.5431.name/music/5.mp3',
+        cover: 'https://www.5431.name/music/1.jpg',
+      },
+    ]
+});
